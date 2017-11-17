0a7e20f1454c5b65680ff2f1591561cd5b95c9bb
lightyears1998 <lightyears1998@hotmail.com>
2017-11-17 20:28:41 +0800

    11.17

---
+++ 12/1223.cpp
@@ -0,0 +1,40 @@
+/* 图像抖动 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        string dithered;
+        cin >> dithered;
+        
+        int x, y;
+        cin >> x >> y;
+        
+        string img = "";
+        string tmp;
+        while (y--) 
+        {
+            cin >> tmp;
+            img += tmp;
+        }
+
+        int cnt = 0;
+        int length = img.length();
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < dithered.length(); j++)
+            {
+                if (dithered[j] == img[i]) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        cout << cnt << endl;
+    }
+}
