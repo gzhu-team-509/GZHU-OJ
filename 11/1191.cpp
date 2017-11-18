06e1a7dd01015c48876df82815081efcc4e5db28
lightyears <lightyears1998@hotmail.com>
2017-11-18 17:26:33 +0800

    11.18 CODE REVIEW

--- 11/1191.cpp
+++ 11/1191.cpp
@@ -7,13 +7,13 @@ int main()
     while (cin >> count)
     {
         long sum = 0, temp;
-        for (int i=0; i < count; i++){
+        for (int i = 0; i < count; i++){
             cin >> temp;
             sum += temp;
         }
         cout.flags(cout.fixed);
         cout.precision(2);
-        cout << (double)sum/count << endl;
+        cout << (double)sum / count << endl;
     }
 
     return 0;
