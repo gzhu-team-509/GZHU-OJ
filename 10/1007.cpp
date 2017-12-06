e04308f406a85ffa744b4ff964b3c20f2a1cb5a8
lightyears1998 <lightyears1998@hotmail.com>
2017-12-06 13:46:14 +0800

    12.06 Hr2

--- 10/1007.cpp
+++ 10/1007.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main()
 {
-	int n;
-	while (cin >> n)
-	{
-		if (n == 1 || n == 2) cout << "-1" << endl;
-		else
-		{
-			for (int i = n; i > 0; i--)
-			{
-				if (i == 1) cout << 1 << endl;
-				else cout << i << " ";
-			}
-		}
-	}
+    long long y, k, n;
+    while (scanf("%lld%lld%lld", &y, &k, &n) == 3)
+    {
+        long long cnt = 0;
+        for (long long tot = y / k * k + k; tot <= n; tot += k)
+        {
+            if (cnt++) printf(" ");
+            printf("%lld", tot - y);
+        }
+        if (cnt == 0) printf("-1\n");
+        else printf("\n");
+    }
 }
