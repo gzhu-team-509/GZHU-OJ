b2b0d20820589bd2e6a5653a4584b1dc87a350f7
lightyears <lightyears1998@hotmail.com>
2018-04-15 11:02:25 +0800

    补充注释

--- 12/1273.cpp
+++ 12/1273.cpp
@@ -11,7 +11,7 @@ int main()
         for (int i = a; i <= b; i++)
         {
             if (i % 2) cubic += i * i * i;
-            else square += i * i;                           // (i % 2 == 1) 没有考虑负数情况
+            else square += i * i;  // 如果写成(i % 2 == 1)，遗漏考虑负数情况
         }
         cout << square << ' ' << cubic << endl;
     }
