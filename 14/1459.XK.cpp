67b193d8df1fd0a1ae1705f2b6c131784b2fb300
lightyears <lightyears1998@hotmail.com>
2018-04-15 11:08:47 +0800

    修正空白错误

--- 14/1459.XK.cpp
+++ 14/1459.XK.cpp
@@ -2,18 +2,18 @@
 #include<cmath>
 #include<cstdio>
 using namespace std;
+
 int main()
 {
     long long a, b, x;
-    while(cin>>a>>b){
-         do
-         {
-             x=a%b;
-             a=b;
-             b=x;
-         }
-          while (x);
-         cout<<a<<endl;
+    while (cin>>a>>b) {
+        do
+        {
+           x=a%b;
+           a=b;
+           b=x;
+        }
+        while (x);
+        cout<<a<<endl;
     }
-
 }
